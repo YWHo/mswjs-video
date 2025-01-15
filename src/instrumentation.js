4bfc0ab3fd47ffcb7cdc7c51291166aa88e2da09
Brian Ho <brian.ho@jucyworld.com>
2025-01-15 16:43:58 +1300

    Add indication that the MSW is running

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -2,5 +2,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { server } = require("./mock/node");
     server.listen();
+    console.log('\nMSW server listening...\n\n');
   }
 }
